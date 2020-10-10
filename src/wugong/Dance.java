8e7a2cab779857437d72b190fc6fd18f9efb4816
ChimmyJimin <2623975431@qq.com>
2020-10-10 22:36:29 +0800

    33333

--- src/wugong/Dance.java
+++ src/wugong/Dance.java
@@ -4,6 +4,7 @@ public class Dance {
 	public static void main(String[] args) {
 		System.out.println("1111111");
 		System.out.println("2222222");
+		System.out.println("3333333");
 		System.out.println("XXXXXXX");
 		System.out.println("YYYYYYY");
 	}
